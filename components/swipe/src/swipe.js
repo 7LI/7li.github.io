a18555fc8c92279649bdd7b7245ab850f4495205
lyz <702368372@qq.com>
2015-03-12 14:41:05 +0800

    Update swipe

--- components/swipe/src/swipe.js
+++ components/swipe/src/swipe.js
@@ -1,13 +1,18 @@
 define(function() {
-  function Swipe(container) {
+  function Swipe(container, option) {
     this.container = container;
     this.wrap = this.container.children[0];
     this.slides = this.wrap.children;
     this.length = this.slides.length;
     this.width = this.container.offsetWidth;
 
-    this.speed = 300;
+    if (!option) option = {};
+    this.option = option;
+    this.speed = option.speed || 300;
+    this.threshould = option.threshould || 160;
+    this.auto = option.auto || 2000;
     this.index = 0;
+    this.offset = 0;
     this.classes = {
       nav: 'swipe-nav',
       navItem: 'swipe-nav-item',
@@ -17,6 +22,9 @@ define(function() {
 
     this.setup(); 
     this.bind();
+
+    this.set('nav', option.nav !== false);
+    this.set('auto', option.auto !== 0);
   }
 
   Swipe.prototype.setup = function() {
@@ -26,21 +34,34 @@ define(function() {
 
     // Set wrap
     this.wrap.style.position = 'relative';
-    this.wrap.style.width = this.width * this.length + 'px';
+    this.wrap.style.overflow = 'visible';
+    this.wrap.style.height = 'auto';
+    this.wrap.style.width = '999999999999999px';
 
     // Set slides   
     for(var i = 0; i < this.length; i++) {
       var slide = this.slides[i];
       slide.style.position = 'relative';
       slide.style.float = 'left';
+      slide.style.height = 'auto';
       this.slides[i].style.width = this.width + 'px';
+      this.slides[i].offset = i * this.width;
     }
   }
 
-  Swipe.prototype.slideTo = function(index, callback, queue) {
+  Swipe.prototype.slideTo = function(index, callback, direction, queue) {
+    // Handling muti-arguments
     if (index > this.length - 1) index = this.length - 1;
     if (index < 0) index = 0;
-    if (typeof callback != 'function') { queue = callback; callback = null };
+    if (typeof callback != 'function' && callback != null) {
+      queue = direction;
+      direction = callback;
+      callback = null;
+    };
+    if (typeof direction != 'string') {
+      queue = direction;
+      direction = 'right';
+    }
 
     // Not sliding and not enqueue, so do nothing
     if (this.sliding && !queue) return;
@@ -49,7 +70,8 @@ define(function() {
     if (this.sliding && queue) {
       var slide = {
         index: index,
-        callback: callback
+        callback: callback,
+        direction: direction
       }
       this.queue.push(slide);
       return;
@@ -64,13 +86,69 @@ define(function() {
     }
 
     this.sliding = true;
-    this.index = index;
     this.callback = callback;
 
-    var offset = this.width * (0 - index);
+    var offset;
+    // Relocate slides
+    var currentSlide = this.slides[this.index];
+    for (var i = 0; i < this.length; i++) {
+      if (i == this.index) continue;
+
+      var slide = this.slides[i];
+      if (direction == 'right') {
+        if (slide.offset < currentSlide.offset) {
+          slide.offset += this.width * this.length;
+          var calculateOffset = slide.offset - this.width * i;
+          slide.style.webkitTransitionDuration = 0;
+          slide.style.webkitTransform = 'translate3D(' + calculateOffset + 'px, 0px, 0px)';
+        }
+      } else if (direction == 'left') {
+        if (slide.offset > currentSlide.offset) {
+          slide.offset -= this.width * this.length;
+          var calculateOffset = slide.offset - this.width * i;
+          slide.style.webkitTransitionDuration = 0;
+          slide.style.webkitTransform = 'translate3D(' + calculateOffset + 'px, 0px, 0px)';
+        }
+      }
+      
+    }
+    // Relocate wrap
+    offset =  this.slides[this.index].offset - this.slides[index].offset;
+    offset += this.offset;
+
+    this.offset = offset;
+    this.index = index;
+
     var style = this.wrap.style;
     style.webkitTransitionDuration = this.speed + 'ms';
     style.webkitTransform = 'translate3D(' + offset + 'px, 0px, 0px)';
+  }
+
+  Swipe.prototype.prev = function(callback, queue) {
+    // Handling muti-arguments
+    if (typeof callback != 'function') { queue = callback; callback = null };
+
+    var index = this.getIndex(queue);
+    if (index <= 0) {
+      index = this.length - 1;
+    } else {
+      index = index - 1;
+    }
+    this.slideTo(index, callback, 'left', queue);
+  }
+
+  Swipe.prototype.next = function(callback, queue) {
+    // Handling muti-arguments
+    if (typeof callback != 'function') { queue = callback; callback = null };
+
+    var index = this.getIndex(queue);
+    if (index >= this.length - 1) {
+      index = 0;
+    } else {
+      index = index + 1;
+    }
+
+    this.slideTo(index, callback, 'right', queue);
   }
 
   Swipe.prototype.stop = function() {
@@ -79,25 +157,60 @@ define(function() {
 
   Swipe.prototype.bind = function() {
     var that = this;
+    var start, delta = {x: 0, y: 0};
     this.events = {
       handleEvent: function(e) {
         switch (e.type) {
-           case 'touchstart': ; break;
-           case 'touchmove': ; break;
-           case 'touchend': ; break;
-           case 'transitionend': 
-             that.sliding = false;
-             that.nav && that.setNav(); // Set nav
+            case 'touchstart':
+              var touches = e.touches[0];
+              start = {
+                x: touches.pageX,
+                y: touches.pageY,
+              };
+              that.container.addEventListener('touchmove', that.events);
+              that.container.addEventListener('touchend', that.events);
+              that.autoplay(false);
+              break;
+            case 'touchmove':
+              var touches = e.touches[0];
+              delta = {
+                x: touches.pageX - start.x,
+                y: touches.pageY - start.y
+              }
+              that.attempSlide(delta);
+              break;
+            case 'touchend': 
+               if (Math.abs(delta.x) > that.threshould) {
+                if (delta.x > 0) {
+                  that.prev();
+                } else {
+                  that.next();
+                }
+              } else {
+                that.cancelSlide();
+              }
+              that.container.removeEventListener('touchmove', that.events);
+              that.container.removeEventListener('touchend', that.events);
+              that.autoplay(that.option.auto !== 0);
+              break;
+            case 'transitionend': 
+              that.sliding = false;
+              that.nav && that.setNav(); // Set nav
 
-             that.callback && that.callback();
-             // Enforce the next slide in queue
-             that.dequeue();
-             break;
-           case 'resize': ; break;
+              that.callback && that.callback();
+              // Enforce the next slide in queue
+              that.dequeue();
+              break;
+            case 'resize':
+              that.width = that.container.offsetWidth;
+              that.setup();
+            break;
          }
       }
     }
     this.container.addEventListener('transitionend', this.events);
+    this.container.addEventListener('touchstart', this.events);
+    window.addEventListener('resize', this.events);
   }
 
   Swipe.prototype.dequeue = function() {
@@ -105,39 +218,13 @@ define(function() {
      nextSlide && this.slideTo(nextSlide.index, nextSlide.callback);
   }
 
-  Swipe.prototype.next = function(callback, queue) {
-    if (typeof callback != 'function') queue = callback;
-
+  Swipe.prototype.getIndex = function(queue) {
     var index = this.index;
     if (queue) {
       var lastSlide = this.queue[this.queue.length - 1];
       if (lastSlide) index = lastSlide.index;
-    }
-
-    if (index >= this.length - 1) {
-      index = 0;
-    } else {
-      index = index + 1;
-    }
-
-    this.slideTo(index, callback, queue);
-  }
-
-  Swipe.prototype.prev = function(callback, queue) {
-    if (typeof callback != 'function') queue = callback;
-
-    var index = this.index;
-    if (queue) {
-      var lastSlide = this.queue[this.queue.length - 1];
-      if (lastSlide) index = lastSlide.index;
-    }
-
-    if (index <= 0) {
-      index = this.length - 1;
-    } else {
-      index = index - 1;
-    }
-    this.slideTo(index, callback, queue);
+    };
+    return index;
   }
 
   Swipe.prototype.set = function(key, value) {
@@ -146,15 +233,32 @@ define(function() {
         if (!this.nav) {
           this.createNav();
         } 
-        if (value == 'on') {
+        if (value == true) {
           this.nav.style.display = 'block';
           this.setNav();
         } else {
           this.nav.style.display = 'none';
         }
-        
+        break;
+      case 'auto':
+        this.autoplay(value);
         break;
     }
+  }
+
+  Swipe.prototype.attempSlide = function(delta) {
+    if (this.sliding) return;
+
+    var style = this.wrap.style;
+    style.webkitTransitionDuration = '0ms';
+    var offset = this.offset + delta.x;
+    style.webkitTransform = 'translate3D(' + offset + 'px, 0px, 0px)';
+  }
+
+  Swipe.prototype.cancelSlide = function() {
+    var style = this.wrap.style;
+    style.webkitTransitionDuration = this.speed + 'ms';
+    style.webkitTransform = 'translate3D(' + this.offset + 'px, 0px, 0px)';
   }
 
   Swipe.prototype.createNav = function() {
@@ -175,9 +279,28 @@ define(function() {
     this.nav.children[this.index].classList.add(this.classes.navOn);
   }
 
+  Swipe.getLength = function() {
+    return this.length;
+  }
+
+  Swipe.prototype.autoplay = function(onOff) {
+    if (this.auto == 0) return;
+
+    clearInterval(this.timer);
+    var that = this;
+    if (onOff) {
+      this.timer = setInterval(function() {
+        that.next();
+      }, this.auto);
+    }
+  }
+
   Swipe.prototype.kill = function() {
     this.container.removeChild(this.nav);
     this.container.removeEventListener('transitionend', this.events);
+    this.container.removeEventListener('touchstart', this.events);
   }
+
+  Swipe.v = Swipe.version = '1.0.0';
   return Swipe;
 })
--- components/swipe/test/tests.js
+++ components/swipe/test/tests.js
@@ -1,19 +1,25 @@
 define(['../src/swipe.js'], function(Swipe) {
   describe('Swipe', function(){
-    var container, swipe, wrap, slides, containerWidth, containerHeight, wrapWidth, navItems;
+    var container, swipe, wrap, slides;
+    function getTransitionOffset(ele){
+        var reg = /translate3d\(([-\d]+)px, 0px, 0px\)/;
+        var str = ele.style.webkitTransform;
+        var match = str.match(reg);
+        var ret = 0;
+        if (match) ret = +match[1];
+        return ret;
+    }
+
     before(function() {
       container = document.getElementById('mySwipe');
       swipe = new Swipe(container);
       wrap = swipe.wrap;
       slides = swipe.slides;
-      containerWidth = container.offsetWidth;
-      wrapWidth = wrap.offsetWidth; 
-      swipe.set('nav', 'on');
-      navItems = container.querySelector('.swipe-nav').children;
     })
 
     describe('setup()', function(){
       it('should set the width and height of the slides as the same with the container', function(){
+        var containerWidth = container.offsetWidth;
         for (var i = 0, len = slides.length; i < len; i++) {
           slides[i].offsetWidth.should.equal(containerWidth);
         }
@@ -21,10 +27,11 @@ define(['../src/swipe.js'], function(Swipe) {
 
       it('should set the width of wrap as the sum of all slides', function() {
         var sum = 0;
+        var wrapWidth = wrap.offsetWidth; 
         for (var i = 0, len = slides.length; i < len; i++) {
           sum += slides[i].offsetWidth;
         }
-        wrapWidth.should.equal(sum);
+        (wrapWidth - sum > 0 ).should.equal(true);
       })
 
     })
@@ -97,30 +104,40 @@ define(['../src/swipe.js'], function(Swipe) {
     })
 
 
-    describe('slideTo(index)', function(){
+    describe('slideTo()', function(){
       it('slideTo(current index), it should do nothing', function() {
-        var oldTransform = swipe.wrap.style.webkitTransform;
-        var oldIdex = swipe.index;
-        swipe.slideTo(swipe.index, function() {
-          var newTransform = swipe.wrap.style.webkitTransform;
-          oldTransform && newTransform.should.equal(oldTransform);
+        var random = Math.floor(Math.random() * swipe.length);
+        var oldOffset, oldIdex;
+        swipe.slideTo(random, function(){
+          oldOffset = getTransitionOffset(wrap);
+          oldIdex = swipe.getCurrentIndex();
+        }, true);
+        
+        swipe.slideTo(random, function() {
+          var newOffset = getTransitionOffset(wrap);
+          newOffset.should.equal(oldOffset);
           swipe.index.should.equal(oldIdex);
         }, true);
       })
 
       it('slideTo(index bigger than max), it should slideTo(max)', function() {
         swipe.slideTo(9999, function() {
-          var offset = swipe.width * (0 - (swipe.length - 1));
-          var transform = swipe.wrap.style.webkitTransform;
-          transform.should.equal('translate3d(' + offset + 'px, 0px, 0px)');
+          var slideInitOffset = 3 * swipe.width;
+          var slideTransitionOffset = getTransitionOffset(slides[3]);
+          var slideCalculateOffset = slideInitOffset + slideTransitionOffset;
+          var wrapOffset =  getTransitionOffset(wrap);
+          slideCalculateOffset.should.equal(-wrapOffset);
           swipe.index.should.equal(3);
         }, true);
       })
 
       it('slideTo(index bigger than min), it should slideTo(min)', function() {
         swipe.slideTo(-1111, function() {
-          var transform = swipe.wrap.style.webkitTransform;
-          transform.should.equal('translate3d(0px, 0px, 0px)');
+          var slideInitOffset = 0;
+          var slideTransitionOffset = getTransitionOffset(slides[0]);
+          var slideCalculateOffset = slideInitOffset + slideTransitionOffset;
+          var wrapOffset =  getTransitionOffset(wrap);
+          slideCalculateOffset.should.equal(-wrapOffset);
           swipe.index.should.equal(0);
         }, true);
       })
@@ -128,9 +145,11 @@ define(['../src/swipe.js'], function(Swipe) {
       it('slideTo(index between min and max, it should slideTo(index))', function() {
         var random = Math.floor(Math.random() * swipe.length);
         swipe.slideTo(random, function() {
-          var offset = swipe.width * (0 - random);
-          var transform = swipe.wrap.style.webkitTransform;
-          transform && transform.should.equal('translate3d(' + offset + 'px, 0px, 0px)');
+          var slideInitOffset = random * swipe.width;;
+          var slideTransitionOffset = getTransitionOffset(slides[random]);
+          var slideCalculateOffset = slideInitOffset + slideTransitionOffset;
+          var wrapOffset =  getTransitionOffset(wrap);
+          slideCalculateOffset.should.equal(-wrapOffset);
           swipe.index.should.equal(random);
         }, true);
       })
@@ -174,13 +193,14 @@ define(['../src/swipe.js'], function(Swipe) {
 
     describe('nav', function(){
       it('size of nav items should be length of slides', function() {
+        var navItems = container.querySelector('.swipe-nav').children;
         navItems.length.should.equal(swipe.length);
       })
 
       it('can switch between show and hide', function() {
-        swipe.set('nav', 'off');
+        swipe.set('nav', false);
         swipe.nav.style.display.should.equal('none');
-        swipe.set('nav', 'on');
+        swipe.set('nav', true);
         swipe.nav.style.display.should.equal('block');
       })
 
@@ -191,20 +211,55 @@ define(['../src/swipe.js'], function(Swipe) {
 
         swipe.slideTo(5, function() {
           swipe.nav.children[3].classList
-                                       .contains('swipe-nav-item-on')
-                                       .should.equal(true);
+                               .contains('swipe-nav-item-on')
+                               .should.equal(true);
         }, true);
         
       })
     })
 
     describe('resize', function() { 
-      it('something', function() {
+      before(function() {
+        window.resizeBy(-2, -2);
+      })
+      after(function() {
+        window.resizeBy(2, 2);
+      })
+      it('should reset the width and height of the slides as the same with the container(runs only in IE))', function(){
+        var containerWidth = container.offsetWidth;
+        for (var i = 0, len = slides.length; i < len; i++) {
+          slides[i].offsetWidth.should.equal(containerWidth);
+        }
+      })
+
+      it('should reset the width of wrap as the sum of all slides(runs only in IE)', function() {
+        var wrapWidth = wrap.offsetWidth;
+        var sum = 0;
+        for (var i = 0, len = slides.length; i < len; i++) {
+          sum += slides[i].offsetWidth;
+        }
+        (wrapWidth - sum > 0).should.equal(true);
       })
     })
 
-     describe('touch', function() {
-      it('something', function() {
+    describe('continuous', function() {
+      it('when it is the last slide, next() should animate to the first slide', function() {
+        swipe.slideTo(swipe.length - 1, true);
+        swipe.next(function(){
+          swipe.index.should.equal(0);
+        }, true);
+      })
+
+      it('when it is the first slide, prev() should animate to the last slide', function() {
+        swipe.slideTo(0, true);
+        swipe.prev(function(){
+          swipe.index.should.equal(3);
+        }, true);
+      })
+    })
+
+    describe('touch', function() {
+      it('should support touch(but I do not know how to test it)', function() {
       })
     })
   })
